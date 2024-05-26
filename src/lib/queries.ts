5c9f888745b6f4e31bb34af09264f39015a5be61
McACE007 <mrinalace@gmail.com>
2024-05-26 13:36:46 +0530

    feat: add initUser Server Action

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -202,3 +202,32 @@ export const deleteAgency = async (agencyId: string) => {
   })
   return response;
 }
+
+export const initUser = async (newUser: Partial<User>) => {
+  const user = await currentUser();
+  if (!user) return;
+
+  const userData = await db.user.upsert({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+    },
+    update: newUser,
+    create: {
+      id: user.id,
+      avatarUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+      name: `${user.firstName} ${user.lastName}`,
+      role: newUser.role || 'SUBACCOUNT_USER',
+    },
+  })
+
+  await clerkClient.users.updateUserMetadata(user.id, {
+    privateMetadata: {
+      role: newUser.role || 'SUBACCOUNT_USER'
+    }
+  })
+
+  return userData;
+}
+
+
