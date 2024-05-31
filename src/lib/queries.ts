8fbad4dd4535ee9afd011d4cdb7dd04320c172ec
McACE007 <mrinalace@gmail.com>
2024-06-01 00:17:57 +0530

    feat: add deletePipeline Server Action

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -565,3 +565,9 @@ export const upsertPipeline = async (
   return response
 }
 
+export const deletePipeline = async (pipelineId: string) => {
+  const response = await db.pipeline.delete({
+    where: { id: pipelineId },
+  })
+  return response
+}
