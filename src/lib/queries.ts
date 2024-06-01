1ee87ca60d3739f5fed9183250bafedacd3570c4
McACE007 <mrinalace@gmail.com>
2024-06-01 17:05:18 +0530

    feat: add a searchContacts Server Action

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -690,3 +690,14 @@ export const getSubAccountTeamMembers = async (subaccountId: string) => {
   })
   return subaccountUsersWithAccess
 }
+
+export const searchContacts = async (searchTerms: string) => {
+  const response = await db.contact.findMany({
+    where: {
+      name: {
+        contains: searchTerms,
+      },
+    },
+  })
+  return response
+}
