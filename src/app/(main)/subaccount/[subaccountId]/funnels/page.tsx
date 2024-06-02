c3d75a643502124cdea781a2b35e5d99dd092e90
McACE007 <mrinalace@gmail.com>
2024-06-02 22:36:33 +0530

    feat: add a funnels page with few private components (Columns, DataTable) and a dynamic route page funnelId which a has its own few private components (FunnelSettings, FunnelStepCard, FunnelSteps)

---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/page.tsx
@@ -0,0 +1,33 @@
+import { getFunnels } from '@/lib/queries'
+import React from 'react'
+import DataTable from './_components/DataTable'
+import { Plus } from 'lucide-react'
+import { columns } from './_components/Columns'
+import FunnelForm from '@/components/forms/FunnelForm'
+
+type Props = {
+  params: { subaccountId: string };
+}
+
+export default async function FunnelsPage({ params }: Props) {
+  const funnels = await getFunnels(params.subaccountId)
+
+  if (!funnels) return null
+
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+          Create Funnel
+        </>
+      }
+      modalChildren={
+        <FunnelForm subAccountId={params.subaccountId}></FunnelForm>
+      }
+      filterValue="name"
+      columns={columns}
+      data={funnels}
+    />
+  )
+}
