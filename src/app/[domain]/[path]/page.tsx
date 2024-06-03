94f7999f82434a2d47c28f0c9e6e06c9fe4d14b5
McACE007 <mrinalace@gmail.com>
2024-06-03 18:48:05 +0530

    feat: add a dynamic domain page to display the page created by users

--- src/app/[domain]/[path]/page.tsx
+++ src/app/[domain]/[path]/page.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import FunnelEditor from '@/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/FunnelEditor'
+import { getDomainContent } from '@/lib/queries'
+import EditorProvider from '@/providers/editor/EditorProvider'
+import { notFound } from 'next/navigation'
+import React from 'react'
 
-export default function page() {
-  return <div>page</div>;
+const Page = async ({
+  params,
+}: {
+  params: { domain: string; path: string }
+}) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1))
+  const pageData = domainData?.FunnelPages.find(
+    (page) => page.pathName === params.path
+  )
+
+  if (!pageData || !domainData) return notFound()
+
+  return (
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={domainData.id}
+    >
+      <FunnelEditor
+        funnelPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
+  )
 }
+
+export default Page
